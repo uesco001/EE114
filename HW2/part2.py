342b7285b6b5020bd303e0e6b332aab6c33da827
uesco001 <uesco001@ucr.edu>
2019-05-17 10:30:09 -0700

    making code prettier

--- HW2/part2.py
+++ HW2/part2.py
@@ -5,42 +5,8 @@ import matplotlib.pyplot as plt
 import random
 import math
 import sys
+from func import BitGenerator, Decoding, Success
 trials = 1000
-
-##  Generates random bits as boolean values #
-def BitGenerator(N):
-    array = []
-    for i in range(0,N):
-        array.append( .5 < random.random()  )
-    return array
-
-#  Decodes the array
-def Decoding(p,N,R,array):
-    ret_array = []
-    for i in range(0,N):
-        bit_result = 0
-        for j in range(0,R):
-            if(p > random.random()):
-                bit_result += -1
-            else:
-                bit_result += 1
-
-        if(bit_result > 0):
-            ret_array.append(array[i])
-        else:
-            ret_array.append(not array[i])
-
-    return ret_array
-
-def Success(p,N,R):
-    array = BitGenerator(N)
-    new_array = Decoding(p,N,R,array)
-    if array == new_array:
-        return 1
-    else:
-        return 0
-
-
 #######################################################
 #                NUMBER 2                             #
 #######################################################i
